b11a1f9b1fe752814eed0ca57353354f2d8be8ba
RolynMeow <122800669+RolynMeow@users.noreply.github.com>
2023-01-23 13:13:55 +0800

    Add files via upload
    Backup Activity-1

---
+++ prelims/Lab-Demo-1/node-example.js
@@ -0,0 +1,12 @@
+//Name: Fernan, Rolyn Joyce F.
+//Module: 3
+//Week 2 and 3: Basics of NodeJs
+
+//First Program
+console.log("Welcome to Earth");
+
+//Creating my first function
+function Display(x){
+    console.log(x);
+}
+Display(200);
